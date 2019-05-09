5a8cce9e8140969949e2220e18a438dbe855609a
Porkins97 <49180437+Porkins97@users.noreply.github.com>
2019-05-09 21:31:09 +0800

    update

---
+++ DinosaurNuggetsUnity/Assets/Scripts/Recipe.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Recipe : MonoBehaviour
+{
+    public GameObject Ingredient;
+    public GameObject Pot;
+    public GameObject Burner;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Pot = this.gameObject;
+        Burner = this.gameObject.transform.parent.gameObject;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(this.gameObject.transform.parent.gameObject != null)
+        {
+            Burner = this.gameObject.transform.parent.gameObject;
+        }
+        else
+        {
+            Burner = null;
+        }
+
+
+    }
+}
