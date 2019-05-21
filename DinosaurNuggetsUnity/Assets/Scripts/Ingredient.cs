b1f60d31e561bf62aecde49c2b696296b498bf76
Brad <brad.power@gmail.com>
2019-05-21 13:07:58 +0800

    Added Recipes and Ingredients

---
+++ DinosaurNuggetsUnity/Assets/Scripts/Ingredient.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public enum IngredientType
+{
+    BREAD,
+    FLOUR,
+    MILK,
+    LETTUCE,
+    ONION_WHOLE,
+    ONION_CHOPPED,
+    STEAK,
+    FISH,
+    EGG,
+    TOMATO_WHOLE,
+    TOMATO_CHOPPED,
+    CHEESE,
+    BROCOLLI,
+    CHICKEN
+}
+
+public class Ingredient : MonoBehaviour
+{
+
+    public IngredientType itype;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AttemptAddToRecipe(Recipe r)
+    {
+        r.AddIngredient(this);
+    }
+}
--- DinosaurNuggetsUnity/Assets/Scripts/Recipe.cs
+++ DinosaurNuggetsUnity/Assets/Scripts/Recipe.cs
@@ -2,23 +2,85 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public class RecipeStep
+{
+    public int amountNeeded;
+    private int amountProvided = 0;
+    public IngredientType ingredient;
+    bool is_complete = false;
+
+    public void ResetStep()
+    {
+        MarkComplete(false);
+        amountProvided = 0;
+    }
+
+    public void MarkComplete(bool val)
+    {
+        is_complete = val;
+    }
+
+    public bool IsComplete()
+    {
+        return is_complete;
+    }
+
+    //returns true if adding the ingredient was OK.
+    //returns false if there's a problem
+    public bool AddIngredient(IngredientType itype)
+    {
+        bool retval = false;
+        if (itype == ingredient)
+        {
+            if (amountProvided < amountNeeded)
+            {
+                amountProvided += 1;
+                if (amountProvided == amountNeeded)
+                {
+                    MarkComplete(true);
+                }
+                retval = true;
+            }
+        }
+        return retval;
+    }
+}
+
 public class Recipe : MonoBehaviour
 {
     public GameObject Ingredient;
     public GameObject Pot;
     public GameObject Burner;
     
+    public RecipeStep[] steps;
+    private int currentStep;
+    public float progress;
+
     // Start is called before the first frame update
     void Start()
     {
-        Pot = this.gameObject;
-        Burner = this.gameObject.transform.parent.gameObject;
+        //Pot = this.gameObject;
+        //Burner = this.gameObject.transform.parent.gameObject;
+        Reset();
+        
+    }
 
+    
+    public void Reset()
+    {
+        foreach (RecipeStep rs in steps)
+        {
+            rs.ResetStep();
+        }
+        currentStep = 0;
+        progress = 0.0f; 
     }
 
     // Update is called once per frame
     void Update()
     {
+        /*
         if(this.gameObject.transform.parent.gameObject != null)
         {
             Burner = this.gameObject.transform.parent.gameObject;
@@ -27,7 +89,41 @@ public class Recipe : MonoBehaviour
         {
             Burner = null;
         }
+        */
 
+    }
 
+    RecipeStep GetCurrentStep()
+    {
+        return steps[currentStep];
+    }
+
+    float UpdateProgress()
+    {
+        if (steps.Length > 0)
+            progress = currentStep / (1.0f * steps.Length);
+        Debug.Log("Progress is " + progress * 100.0f + "%");
+        return progress;
+    }
+
+    public bool AddIngredient(Ingredient ingredient)
+    {
+        bool correct = GetCurrentStep().AddIngredient(ingredient.itype);
+        if (correct)
+        {
+            Debug.Log("added successfully");
+            if (GetCurrentStep().IsComplete())
+            {
+                Debug.Log("This Step is Complete");
+                currentStep += 1;
+                UpdateProgress();
+            }
+        }
+        else
+        {
+            Debug.Log("Recipe Failed");
+            Reset();
+        }
+        return correct;
     }
 }
