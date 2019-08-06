22907fc48175b5d6d42d11fb5fcd079b81e536da
Porkins97 <49180437+Porkins97@users.noreply.github.com>
2019-08-06 11:56:34 +0800

    BinUpdate

--- DinosaurNuggetsUnity/Assets/Scripts/Bin.cs
+++ DinosaurNuggetsUnity/Assets/Scripts/Bin.cs
@@ -7,6 +7,8 @@ public class Bin : MonoBehaviour
 
     public GameObject TrashCan;
     public GameObject Ingredient;
+    public GameObject Position;
+    Rigidbody IngredientRigidBody;
 
     Collider ThisCollider;
 
@@ -16,6 +18,7 @@ public class Bin : MonoBehaviour
         TrashCan = this.gameObject;
         ThisCollider = this.gameObject.GetComponent<Collider>();
         Ingredient = null;
+        Position = TrashCan.gameObject.transform.GetChild(0).gameObject;
     }
 
     // Update is called once per frame
@@ -30,15 +33,21 @@ public class Bin : MonoBehaviour
         {
             Debug.Log("Hit Bin");
             Ingredient = collision.gameObject;
+            IngredientRigidBody = Ingredient.GetComponent<Rigidbody>();
 
             if(Ingredient.GetComponent<PickupTest>().ThisItemIsBeingCarried == true)
             {
                 Debug.Log("This Item is being carried");
                 Ingredient.GetComponent<PickupTest>().Drop();
             }
+            IngredientRigidBody.isKinematic = false;
+            Ingredient.GetComponent<Collider>().enabled = false;
+            Ingredient.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+            Ingredient.transform.position = Position.transform.position;
+            IngredientRigidBody.velocity = new Vector3(0f, -0.1f, 0f);
 
-            GameObject.Destroy(Ingredient);
-
+            GameObject.Destroy(Ingredient,2.00f);
+            
         }
     }
 }
