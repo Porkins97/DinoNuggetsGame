cdf072dc17d0f3df5dc2fb2637929485fe0847b4
Jaron Wilding <jaronwilding95@gmail.com>
2019-08-07 15:13:47 +0800

    Running quick tests

---
+++ DinosaurNuggetsUnity/Assets/Scripts/Jaron/PlayerMoveTest_001.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMoveTest_001 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Transform player;
+    void Start()
+    {
+        player = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
