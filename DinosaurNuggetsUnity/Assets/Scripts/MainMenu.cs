8e020a95e4a87eb0fdbce2be90c66c7f1bbf14a3
Porkins97 <49180437+Porkins97@users.noreply.github.com>
2019-08-20 11:23:09 +0800

    Basic Main Menu

---
+++ DinosaurNuggetsUnity/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public Button yourButton;
+
+	void Start () 
+    {
+		Button btn = yourButton.GetComponent<Button>();
+		btn.onClick.AddListener(TaskOnClick);
+	}
+
+	void TaskOnClick()
+    {
+		Debug.Log ("You have clicked the button!");
+        ChangeScene();
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+   public void ChangeScene()
+    {
+        SceneManager.LoadScene("DinoNuggetsPrototypeScene2");
+        Debug.Log("Changed Scene");
+    }
+}
