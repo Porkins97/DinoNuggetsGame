09b6cac3fe9098c769d6c5c0a211b1f1ce5345c6
Porkins97 <49180437+Porkins97@users.noreply.github.com>
2019-08-20 14:09:01 +0800

    MainMenu Mid Update
    Main Menu screen works but is basic. three different screens for main, level select and credits.

--- DinosaurNuggetsUnity/Assets/Scripts/MainMenu.cs
+++ DinosaurNuggetsUnity/Assets/Scripts/MainMenu.cs
@@ -6,29 +6,105 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    public Button yourButton;
+    public Button PlayButton;
+    public Button LevelSelectButton;
+    public Button CreditsButton;
+    public Button HomeLevel;
+    public Button HomeCredits;
+
+    string Kitchen = "DinoNuggetsPrototypeScene2";
+
+    public GameObject Homescreen;
+    public GameObject LevelSelectScreen;
+    public GameObject CreditsScreen;
+
+    bool MainMenuActive = true;
+    bool LevelSelectActive = false;
+    bool CreditsActive = false;
 
 	void Start () 
     {
-		Button btn = yourButton.GetComponent<Button>();
-		btn.onClick.AddListener(TaskOnClick);
-	}
+		Button btnPlay = PlayButton.GetComponent<Button>();
+		btnPlay.onClick.AddListener(TaskOnClickPlay);
 
-	void TaskOnClick()
-    {
-		Debug.Log ("You have clicked the button!");
-        ChangeScene();
-	}
+        Button btnSelect = LevelSelectButton.GetComponent<Button>();
+        btnSelect.onClick.AddListener(TaskOnClickLevel);
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Button btnCredit = CreditsButton.GetComponent<Button>();
+        btnCredit.onClick.AddListener(TaskOnClickCredit);
+
+        Button btnHomeLevel = HomeLevel.GetComponent<Button>();
+        btnHomeLevel.onClick.AddListener(TaskOnClickHome);
+
+        Button btnHomeCredits = HomeCredits.GetComponent<Button>();
+        btnHomeCredits.onClick.AddListener(TaskOnClickHome);
+
+        UpdateUI();
     }
 
-   public void ChangeScene()
+	void TaskOnClickPlay()
     {
-        SceneManager.LoadScene("DinoNuggetsPrototypeScene2");
+		Debug.Log ("You have clicked the Play button!");
+        ChangeScene(Kitchen);
+	}
+    void TaskOnClickLevel()
+    {
+        Debug.Log("You have clicked the Level Select button!");
+        MainMenuActive = false;
+        LevelSelectActive = true;
+        CreditsActive = false;
+        UpdateUI();
+    }
+    void TaskOnClickCredit()
+    {
+        Debug.Log("You have clicked the Credit button!");
+        MainMenuActive = false;
+        LevelSelectActive = false;
+        CreditsActive = true;
+        UpdateUI();
+    }
+    void TaskOnClickHome()
+    {
+        Debug.Log("You have clicked the Home button!");
+        MainMenuActive = true;
+        LevelSelectActive = false;
+        CreditsActive = false;
+        UpdateUI();
+    }
+
+   public void ChangeScene(string SceneName)
+    {
+        SceneManager.LoadScene(SceneName);
         Debug.Log("Changed Scene");
+    }
+
+    void UpdateUI()
+    {
+        if(MainMenuActive == true)
+        {
+            Homescreen.SetActive(true);
+        }
+        else
+        {
+            Homescreen.SetActive(false);
+        }
+
+        if(LevelSelectActive == true)
+        {
+            LevelSelectScreen.SetActive(true);
+        }
+        else
+        {
+            LevelSelectScreen.SetActive(false);
+        }
+
+        if(CreditsActive == true)
+        {
+            CreditsScreen.SetActive(true);
+        }
+        else
+        {
+            CreditsScreen.SetActive(false);
+        }
     }
 }
