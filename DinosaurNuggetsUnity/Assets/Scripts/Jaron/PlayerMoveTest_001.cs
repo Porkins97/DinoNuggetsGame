2e0c507eb392dca05918b61e5e12889e64f3f3ed
Jaron Wilding <jaronwilding95@gmail.com>
2019-08-27 10:25:38 +0800

    Fixed models

--- DinosaurNuggetsUnity/Assets/Scripts/Jaron/PlayerMoveTest_001.cs
+++ DinosaurNuggetsUnity/Assets/Scripts/Jaron/PlayerMoveTest_001.cs
@@ -4,16 +4,58 @@ using UnityEngine;
 
 public class PlayerMoveTest_001 : MonoBehaviour
 {
-    // Start is called before the first frame update
-    private Transform player;
+    private Animator _anim;
+    private Transform _player;
+
+    [SerializeField] private float _playerSpeed;
+
+
     void Start()
     {
-        player = GetComponent<Transform>();
+        _player = GetComponent<Transform>();
+        _anim = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
+    {
+
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        ApplyInput();
+
+        float currentSpeed = 0.0f;
+
+        if (h != 0 || v != 0)
+        {
+            _anim.SetFloat("animWalk", Mathf.Clamp01(h + v));
+        }
+        /*
+        float speed = 0.0f;
+        if (Input.GetKey(KeyCode.W))
+        {
+            speed = 1.0f;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            speed = 4.0f;
+        }
+        */
+      
+    }
+
+    private void ApplyInput()
+    {
+        Move();
+        Turn();
+    }
+    private void Move()
     {
         
     }
+    private void Turn()
+    {
+
+    }
+
+
 }
