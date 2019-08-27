4e41ddb3fcd56c34339627ef179858b786e3a8a9
Jaron Wilding <jaronwilding95@gmail.com>
2019-08-27 13:02:16 +0800

    FixedScripts

---
+++ DinosaurNuggetsUnity/Assets/Scenes/Scenes_Jaron/PickupScript.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupScript : MonoBehaviour
+{
+    public void PickItUp(Collider hand, Collider obj)
+    {
+
+    }
+}
