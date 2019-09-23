ae9b5dd101321427690414d917ce2bd93985f9ec
Jaron Wilding <jaronwilding95@gmail.com>
2019-09-23 13:48:27 +0800

    Updated scripts (again)

--- DinosaurNuggetsUnity/Assets/Scripts/CookingScripts/SO_Recipes.cs
+++ DinosaurNuggetsUnity/Assets/Scripts/CookingScripts/SO_Recipes.cs
@@ -6,6 +6,6 @@ using UnityEditor;
 [CreateAssetMenu(fileName = "", menuName = "Dino Nuggets/Meals")]
 public class SO_Recipes : ScriptableObject
 {
-    public List<GameObject> ingredients;
+    public List<SO_Ingredients> ingredients;
     public GameObject meal;
 }
