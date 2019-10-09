997598448cf27c64bc599d32b0164d5013eeb283
Porkins97 <49180437+Porkins97@users.noreply.github.com>
2019-10-09 18:08:55 +0800

    Added Random hat element to game scenes
    Going to start work on choosing which hat you wear soon.
    Scene needs to have a prefab called "Wearables" in it at this stage to work, need to fix this

---
+++ DinosaurNuggetsUnity/Assets/Scripts/DinoCustomise.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public enum HatType
+{
+    None = 0,
+    Cowboy = 1,
+    Crown = 2,
+    Helicopter = 3,
+    RussianSmall = 4, 
+    RussianLarge = 5,
+    Shark = 6,
+    Snapback = 7,
+    SpaceHelmet = 8,
+    TophatSmall = 9,
+    TophatLarge = 10,
+    Chefs = 11,
+    Party = 12
+}
+
+
+public class DinoCustomise : MonoBehaviour
+{
+    [SerializeField] public HatType hatType;
+}
