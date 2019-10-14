51742bbe9b94b783791683f2a79e153b1e990477
Porkins97 <49180437+Porkins97@users.noreply.github.com>
2019-10-14 13:45:25 +0800

    Added Hat Materials + Outdoor Scene Update

--- DinosaurNuggetsUnity/Assets/Scripts/ApplyHat.cs
+++ DinosaurNuggetsUnity/Assets/Scripts/ApplyHat.cs
@@ -5,38 +5,23 @@ using UnityEngine;
 public class ApplyHat : MonoBehaviour
 {
     int hatNum;
-    public GameObject hatCheck;
     public HatType hat;
     public GameObject[] hats;
     public bool randomHat;
-    // Start is called before the first frame update
+
     void Start()
     {
-        hats = GameObject.FindGameObjectsWithTag("Wearable");
         if (randomHat == true)
         {
-            hatNum = Random.Range(0, 12);
+            hatNum = Random.Range(0, hats.Length);
             hat = (HatType)hatNum;
-            
-            for(int i = 0; i< hats.Length; i++)
-            {
-                if (hats[i].GetComponent<DinoCustomise>().hatType == hat)
-                {
-                    GameObject appliedHat = Instantiate(hats[i], this.gameObject.transform);
-                    appliedHat.transform.parent = this.gameObject.transform;
-                    Destroy(hats[i]);
-                }
-                else
-                {
-                    Destroy(hats[i]);
-                }
-            }
         }       
-        else
+        for(int i = 0; i< hats.Length; i++)
         {
-            for(int i =0; i < hats.Length; i++)
+            if (hats[i].GetComponent<DinoCustomise>().hatType == hat)
             {
-                Destroy(hats[i]);
+                GameObject appliedHat = Instantiate(hats[i], this.gameObject.transform);
+                appliedHat.transform.parent = this.gameObject.transform;
             }
         }
     }
